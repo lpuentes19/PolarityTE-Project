f08fdf867c14060926ba3ac5041bde8d8ee0153a
lpuentes19 <luey.puentes@gmail.com>
2018-12-05 10:25:57 -0700

    Implemented FetchedResults controller in the UserTableView. It now monitors and observes the managed object context for any changes to the objects and updates it automatically. Because of that, deleted the users array property and also the viewWillAppear method. It will take care of fetching those objects for us only when needed.

--- PolarityTE_Project/View Controller/UserTableViewController.swift
+++ PolarityTE_Project/View Controller/UserTableViewController.swift
@@ -9,40 +9,41 @@
 import UIKit
 import CoreData
 
-class UserTableViewController: UITableViewController {
+class UserTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
     
-    var users: [User] {
-        let userRequest: NSFetchRequest<User> = User.fetchRequest()
+    lazy var fetchedResultsController: NSFetchedResultsController<User> = {
+        let fetchRequest: NSFetchRequest<User> = User.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
         let moc = CoreDataStack.shared.mainContext
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                             managedObjectContext: moc,
+                                             sectionNameKeyPath: nil,
+                                             cacheName: nil)
+        frc.delegate = self
         
         do {
-            return try moc.fetch(userRequest)
+            try frc.performFetch()
         } catch {
-            print("Error fetching Users, error: \(error.localizedDescription)")
-            return []
+            print("Error fetching user objects using fetchResultsController, error: \(error.localizedDescription)")
         }
-    }
+        
+        return frc
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        tableView.reloadData()
-    }
 
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+        return fetchedResultsController.fetchedObjects?.count ?? 0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath) as! UserTableViewCell
 
-        let user = users[indexPath.row]
+        let user = fetchedResultsController.object(at: indexPath)
         
         cell.user = user
 
@@ -52,7 +53,7 @@ class UserTableViewController: UITableViewController {
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            let user = users[indexPath.row]
+            let user = fetchedResultsController.object(at: indexPath)
             let moc = CoreDataStack.shared.mainContext
             moc.delete(user)
             
@@ -65,13 +66,45 @@ class UserTableViewController: UITableViewController {
             tableView.reloadData()
         }    
     }
+    
+    // MARK: - NSFetchedResultsController Delegate Methods
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+            
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+        case .move:
+            guard let oldIndexPath = indexPath,
+                let newIndexPath = newIndexPath else { return }
+            tableView.deleteRows(at: [oldIndexPath], with: .automatic)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+            
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+    }
 
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "toUserDetailVC" {
             let userDetailVC = segue.destination as! UserDetailViewController
             if let indexPath = tableView.indexPathForSelectedRow {
-                let user = users[indexPath.row]
+                let user = fetchedResultsController.object(at: indexPath)
                 userDetailVC.user = user
             }
         }
